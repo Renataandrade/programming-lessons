a7731b31a8b88481c607bb97c230296d1acaffbd
renataandrade <knattly@gmail.com>
2022-07-09 20:21:44 -0300

    init

---
+++ scripts/main.js
@@ -0,0 +1,45 @@
+const teste = null;
+
+console.log(`
+    A variável "teste" possui valor ${teste}, 
+    e é do tipo ${typeof teste}`
+);
+
+// Dados os valores A e B
+const A = 4;
+const B = 2;
+
+// Retorne a Soma de A e B
+const resultadoDaSoma = A + B;
+
+// Retorne a Subtração de A e B
+const resultadoDaSubtracao = 0;
+
+// Retorne a Multiplicacao de A e B
+const resultadoDaMultiplicacao = 0;
+
+// Retorne a Divisao de A e B
+const resultadoDaDivisao = 0;
+
+// Retorne o Resto da Divisao de A e B
+const resultadoDoRestoDaDivisao = 0;
+
+console.log(`
+    A soma de ${A} e ${B} é igual a ${resultadoDaSoma}`
+);
+
+console.log(`
+    A subtração de ${A} e ${B} é igual a ${resultadoDaSubtracao}`
+);
+
+console.log(`
+    A multiplicação de ${A} e ${B} é igual a ${resultadoDaMultiplicacao}`
+);
+
+console.log(`
+    A divisão de ${A} por ${B} é igual a ${resultadoDaDivisao}`
+);
+
+console.log(`
+    O resto da divisão de ${A} por ${B} é igual a ${resultadoDoRestoDaDivisao}`
+);
